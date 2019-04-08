975fbbc505c35b0d3c3c86fe59166bb646e9efbf
richardnorthrup <northr2@rpi.edu>
2019-04-08 16:54:06 -0400

    Created new package

---
+++ codebuddy-package/lib/codebuddy-package.js
@@ -0,0 +1,49 @@
+'use babel';
+
+import CodebuddyPackageView from './codebuddy-package-view';
+import { CompositeDisposable } from 'atom';
+
+export default {
+
+  codebuddyPackageView: null,
+  modalPanel: null,
+  subscriptions: null,
+
+  activate(state) {
+    this.codebuddyPackageView = new CodebuddyPackageView(state.codebuddyPackageViewState);
+    this.modalPanel = atom.workspace.addModalPanel({
+      item: this.codebuddyPackageView.getElement(),
+      visible: false
+    });
+
+    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+    this.subscriptions = new CompositeDisposable();
+
+    // Register command that toggles this view
+    this.subscriptions.add(atom.commands.add('atom-workspace', {
+      'codebuddy-package:toggle': () => this.toggle()
+    }));
+  },
+
+  deactivate() {
+    this.modalPanel.destroy();
+    this.subscriptions.dispose();
+    this.codebuddyPackageView.destroy();
+  },
+
+  serialize() {
+    return {
+      codebuddyPackageViewState: this.codebuddyPackageView.serialize()
+    };
+  },
+
+  toggle() {
+    console.log('CodebuddyPackage was toggled!');
+    return (
+      this.modalPanel.isVisible() ?
+      this.modalPanel.hide() :
+      this.modalPanel.show()
+    );
+  }
+
+};
---
+++ codebuddy-package/spec/codebuddy-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodebuddyPackageView from '../lib/codebuddy-package-view';
+
+describe('CodebuddyPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
